e0f8731eddc4edb4760bff6c002cf3b91e802e4c
Stephen Balakirsky <stephen.balakirsky@gtri.gatech.edu>
2016-04-15 16:35:10 -0400

    Added case statement

--- src/test1.cpp
+++ src/test1.cpp
@@ -4,6 +4,18 @@ int main()
 	for (int i = 0; i < 10; ++i)
 	{
 		std::cout << i << std::endl;
+		switch ( i )
+		{
+		case 1:
+			std::cout << "one" << std::endl;
+			break;
+		case 2:
+			std::cout << "two" << std::endl;
+			break;
+		default:
+			std::cout << "not one or two" << std::endl;
+			break;
+		}
 	}
 	return 0;
 }
